af8319c49f2b5f7806fd1ad264e6564a5b58a0bd
Mohamed Ebrahim <jshdheh33i38@gmail.com>
2026-08-04 21:45:47 +0300

    feat: add comment model schema

---
+++ Backend/models/messageModel.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+const messageSchema = new mongoose.Schema(
+  {
+    project: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Project",
+      required: [true, "Message must belong to a project."],
+    },
+    sender: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: [true, "Message must have a sender."],
+    },
+    text: {
+      type: String,
+      required: [true, "Message text cannot be empty."],
+      trim: true,
+    },
+    isRead: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+const Message = mongoose.model("Message", messageSchema);
+module.exports = Message;
